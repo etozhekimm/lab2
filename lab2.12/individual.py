a3c7415e9bdb259419f7c3fd05f13d0be862bfe4
Daniil <tankopojiratel@gmail.com>
2022-02-02 04:34:48 +0300

    Лабораторная 2.12 выполнена

---
+++ lab2.12/individual.py
@@ -0,0 +1,15 @@
+def start_func(text1, text2):
+    return text1.split(), text2.split()
+
+
+def decorated(func):
+    def decorated_m(text1, text2):
+        data = func(text1, text2)
+        return dict(zip(*data))
+    return decorated_m
+
+
+start_func = decorated(start_func)
+x = input()
+y = input()
+print(start_func(x, y))
