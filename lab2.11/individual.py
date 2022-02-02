30a7338901eaf4c94ad3daf9a06a64d434c0fd8a
Daniil <tankopojiratel@gmail.com>
2022-02-02 04:09:16 +0300

    Лабораторная 2.11 выполнена

---
+++ lab2.11/individual.py
@@ -0,0 +1,14 @@
+def func(func_type: str):
+    def inner(value):
+        gen = (e for e in value.split())
+        if func_type == 'list':
+            return list(gen)
+        return tuple(gen)
+
+    return inner
+
+
+if __name__ == '__main__':
+    x = input()
+    y = input()
+    print(func(x)(y))
