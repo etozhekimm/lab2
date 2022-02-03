63d91c613a3274aff211165ed5dd8da331b3af87
Daniil <tankopojiratel@gmail.com>
2022-02-03 15:08:20 +0300

    Правки в лабораторной 2.3

--- lab2.3/individual2.py
+++ lab2.3/individual2.py
@@ -1,19 +1,23 @@
-message = str(input("Введите строку:")) + "О"
-rule1 = "чя"
-rule2 = "щя"
-mistake = 0
-right = 1
-print("\"", end='')
-for i in range(len(message) - 1):
-    if mistake == 0:
-        print(message[i], end='')
-        if ((message[i] + message[i + 1]) == rule1) or ((message[i] + message[i + 1]) == rule2):
-            mistake = 1
-            right = 0
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    message = str(input("Введите строку:")) + "О"
+    rule1 = "чя"
+    rule2 = "щя"
+    mistake = 0
+    right = 1
+    print("\"", end='')
+    for i in range(len(message) - 1):
+        if mistake == 0:
+            print(message[i], end='')
+            if ((message[i] + message[i + 1]) == rule1) or ((message[i] + message[i + 1]) == rule2):
+                mistake = 1
+                right = 0
+        else:
+            print('а', end='')
+            mistake = 0
+    if right:
+        print("\" — это верное предложение!")
     else:
-        print('а', end='')
-        mistake = 0
-if right:
-    print("\" — это верное предложение!")
-else:
-    print("\" — наверное, правильно так.")
+        print("\" — наверное, правильно так.")
--- lab2.3/individual3.py
+++ lab2.3/individual3.py
@@ -1,10 +1,14 @@
-message = str(input("Задайте предложение:"))
-letter = str(input("Задайте букву:"))
-result = ""
-last = 0
-for i in range(len(message) - 1, -1, -1):
-    result += message[i]
-    if (message[i] == "и") and (not last):
-        result += letter
-        last = 1
-print(result[::-1])
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    message = str(input("Задайте предложение:"))
+    letter = str(input("Задайте букву:"))
+    result = ""
+    last = 0
+    for i in range(len(message) - 1, -1, -1):
+        result += message[i]
+        if (message[i] == "и") and (not last):
+            result += letter
+            last = 1
+    print(result[::-1])
