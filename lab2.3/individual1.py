9990009657639a52a3a3d2adf1db1396c8e019d6
Daniil <tankopojiratel@gmail.com>
2022-02-03 15:17:17 +0300

    Правки в лабораторной 2.3

--- lab2.3/individual1.py
+++ lab2.3/individual1.py
@@ -1,11 +1,15 @@
-word = str(input("Введите строку:"))
-part1 = str(input("Введите первый символ:"))
-part2 = str(input("Введите второй символ:"))
-print("\n", "Координаты вхождений первого символа:", end='')
-for i in range(len(word) - 1):
-    if word[i] == part1:
-        print(' ', i, end='')
-print("\n", "Координаты вхождений второго символа:", end='')
-for i in range(len(word) - 1):
-    if word[i] == part2:
-        print(' ', i, end='')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    word = str(input("Введите строку:"))
+    part1 = str(input("Введите первый символ:"))
+    part2 = str(input("Введите второй символ:"))
+    print("\n", "Координаты вхождений первого символа:", end='')
+    for i in range(len(word) - 1):
+        if word[i] == part1:
+            print(' ', i, end='')
+    print("\n", "Координаты вхождений второго символа:", end='')
+    for i in range(len(word) - 1):
+        if word[i] == part2:
+            print(' ', i, end='')
