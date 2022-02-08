e19a3948948df7c85cb7aa456019119366adb829
Daniil <tankopojiratel@gmail.com>
2022-02-08 22:08:27 +0300

    Правки в лабораторной 2.9

--- lab2.9/individual.py
+++ lab2.9/individual.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 def summary(mas, inx):
     if int(mas[inx]) > 0:
         return summary(mas, inx + 1) + int(mas[inx])
@@ -5,5 +8,6 @@ def summary(mas, inx):
         return 0
 
 
-ms = input()
-print(summary(ms.split(), 0))
+if __name__ == '__main__':
+    ms = input()
+    print(summary(ms.split(), 0))
