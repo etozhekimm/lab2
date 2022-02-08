69ab8dc9b379feda1e1ea5924b9b268343e236cf
Daniil <tankopojiratel@gmail.com>
2022-02-08 21:46:15 +0300

    Правки в лабораторной 2.7

--- lab2.7/task1.py
+++ lab2.7/task1.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 if __name__ == "__main__":
     gl = set("aeiouy")
     string = set(input("Введите строку: ").lower())
     print(gl & string)
+
--- lab2.7/task2.py
+++ lab2.7/task2.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 if __name__ == "__main__":
     first_string = set(input("Введите первую строку: ").lower())
     second_string = set(input("Введите вторую строку: ").lower())
