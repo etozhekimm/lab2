0c5ed1b0c066ac0c34d0e368f6c33c4111344bb6
Daniil <tankopojiratel@gmail.com>
2022-02-08 20:42:17 +0300

    Правки в лабораторной 2.4

--- lab2.4/individual1.py
+++ lab2.4/individual1.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 
 if __name__ == '__main__':
@@ -6,11 +9,11 @@ if __name__ == '__main__':
         print("Заданный список пуст", file=sys.stderr)
         exit(1)
 
-sum = 0
-amount = 0
-for i in range(10):
-    if ((a[i] % 7) == 0) and (a[i] < 0):
-        amount += 1
-        sum += a[i]
-print("Количество отрицательных элементов кратных семи: ", amount)
-print("Их сумма: ", sum)
+    sum = 0
+    amount = 0
+    for i in range(10):
+        if ((a[i] % 7) == 0) and (a[i] < 0):
+            amount += 1
+            sum += a[i]
+    print("Количество отрицательных элементов кратных семи: ", amount)
+    print("Их сумма: ", sum)
--- lab2.4/individual2.py
+++ lab2.4/individual2.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 
 if __name__ == '__main__':
@@ -5,26 +8,27 @@ if __name__ == '__main__':
     if not lst:
         print("Заданный список пуст", file=sys.stderr)
         exit(1)
-a = int(input("Введите левую границу интервала (a):"))
-b = int(input("Введите правую границу интервала (b):"))
+    a = int(input("Введите левую границу интервала (a):"))
+    b = int(input("Введите правую границу интервала (b):"))
 
-new = []
-result = 0
-plus = 0
-maximum = 0
-last = 0
+    new = []
+    result = 0
+    plus = 0
+    maximum = 0
+    last = 0
 
-for i in range(len(lst)):
-    if abs(lst[maximum]) <= abs(lst[i]):
-        maximum = i
-    if plus == 1:
-        result += lst[i]
-    if lst[i] > 0:
-        plus = 1
-    if int(lst[i]) in range(a, b + 1):
-        lst[i], lst[last] = lst[last], lst[i]
-        last += 1
+    for i in range(len(lst)):
+        if abs(lst[maximum]) <= abs(lst[i]):
+            maximum = i
+        if plus == 1:
+            result += lst[i]
+        if lst[i] > 0:
+            plus = 1
+        if int(lst[i]) in range(a, b + 1):
+            lst[i], lst[last] = lst[last], lst[i]
+            last += 1
 
-print("Элемент номер", maximum + 1, " - максимальный по модулю")
-print(result, " - Сумма элементов после первого положительного")
-print("Преобразованный список: ", lst)
+    print("Элемент номер", maximum + 1, " - максимальный по модулю")
+    print(result, " - Сумма элементов после первого положительного")
+    print("Преобразованный список: ", lst)
+
